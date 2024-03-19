76cc4e695d195c74aad741faa8899f2de531e5eb
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-03-20 03:13:44 +0600

    Update misakaomoji.py

--- misakaomoji.py
+++ misakaomoji.py
@@ -1,50 +1,37 @@
 import openai
 import streamlit as st
 
-# Set Streamlit page configuration
+
 st.set_page_config(
     page_title="MisaKaomoji 🎂",
     page_icon="favicon.ico",
     layout="wide",
     initial_sidebar_state="expanded"
+
 )
 
-# Title and description
 st.title("MisaKaoMoji")
 st.text("Your emotions, with an AI-powered kawaii twist")
 
-# Set up OpenAI API
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Initialize messages if not present
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Initialize openai_model if not present
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Display previous messages
+print(st.session_state["openai_model"])  # Debugging
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Get user input and generate response
-if prompt := st.chat_input("What's up?"):
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Instruction message for Misakaomoji
-    instruction_message = (
-        "Welcome to MisaKaomoji! I'm Misakaomoji, your kawaii kaomoji creator. "
-        "Please express your emotions, and I'll generate adorable kaomoji for you."
-    )
-    st.session_state.messages.append({"role": "assistant", "content": instruction_message})
-    with st.chat_message("assistant"):
-        st.markdown(instruction_message)
-
-    # Generate responses using OpenAI's Chat Completion API
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
