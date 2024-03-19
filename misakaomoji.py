cb3c7cb6ee4eb546a10553add9a9b955126549d8
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-03-20 02:06:48 +0600

    Update misakaomoji.py

--- misakaomoji.py
+++ misakaomoji.py
@@ -1,6 +1,6 @@
 import openai
 import streamlit as st
-st.beta_set_page_config(page_title='MisaKaomoji - Your emotions, with an AI-powered kawaii twist')
+
 st.title("MisaKaoMoji")
 st.text("Your emotions, with an AI-powered kawaii twist")
 
